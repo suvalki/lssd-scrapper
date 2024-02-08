743672d5327a4f9e594b7acba7f21e4d9454c125
Dmitriy K <dmitriykud-07@bk.ru>
2024-02-08 14:34:01 +0300

    fix MantineProvidersv4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import '@mantine/core/styles.css';
 
-import {ColorSchemeScript} from '@mantine/core';
+import {ColorSchemeScript, createTheme, MantineProvider} from '@mantine/core';
 import Providers from "@/utils/components/Providers";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import React from "react";
 
 export const metadata = {
     title: "Почта",
@@ -10,12 +12,26 @@ export const metadata = {
 export default function RootLayout({children}: {
     children: React.ReactNode;
 }) {
+    const queryClient = new QueryClient()
+    const theme = createTheme({
+
+        primaryColor: "green",
+        colors: {
+            green: ["#9FFF40", "#84FF09", "#76ED00", "#69D100", "#5BB600", "#4D9B00", "#408000", "#3B7600", "#376D00", "#326400"],
+
+        }
+    })
+
     return (
         <html lang="en">
 
         <body>
         <ColorSchemeScript/>
-        <Providers>{children}</Providers>
+        <QueryClientProvider client={queryClient}>
+            <MantineProvider theme={theme} defaultColorScheme={"dark"}>
+                {children}
+            </MantineProvider>
+        </QueryClientProvider>
         </body>
         </html>
     );
