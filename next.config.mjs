5e89936b55bdcfbd112fd06df0046cda0703015a
Dmitriy K <dmitriykud-07@bk.ru>
2024-02-08 14:18:35 +0300

    fix MantineProviders

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        optimizePackageImports: [
+            "@mantine/*",
+            "@tanstack/react-query",
+        ]
+    }
+};
 
 export default nextConfig;
