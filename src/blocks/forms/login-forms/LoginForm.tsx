b76d9c38e4c95928fb13c232a7e2c7d6516fd538
Dmitriy K <dmitriykud-07@bk.ru>
2024-02-08 15:27:23 +0300

    fix MantineProvidersv9

---
+++ src/blocks/forms/login-forms/LoginForm.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React from "react"
+import {Button, Stack, TextInput} from "@mantine/core";
+import {Lock, User} from "lucide-react";
+
+export default function LoginForm() {
+    return (
+        <div style={{
+            width: "100%",
+        }}>
+            <Stack>
+                <TextInput w={"100%"} placeholder={"Логин"} leftSection={<User size={20} />} />
+                <TextInput w={"100%"} placeholder={"Пароль"} type={"password"} leftSection={<Lock size={20} />} />
+                <Button >Войти</Button>
+            </Stack>
+        </div>
+    )
+}
