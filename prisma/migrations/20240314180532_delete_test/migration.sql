1927f43ac3098c2f81068e58adaa92c12a767e79
Dmitriy K <dmitriykud-07@bk.ru>
2024-03-14 21:07:34 +0300

    delete-test

---
+++ prisma/migrations/20240314180532_delete_test/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `test` on the `ForumAccount` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_ForumAccount" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "login" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdId" INTEGER NOT NULL,
+    "active" BOOLEAN NOT NULL,
+    "sid" TEXT NOT NULL,
+    "lastCheck" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "ForumAccount_createdId_fkey" FOREIGN KEY ("createdId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_ForumAccount" ("active", "createdAt", "createdId", "id", "lastCheck", "login", "password", "sid") SELECT "active", "createdAt", "createdId", "id", "lastCheck", "login", "password", "sid" FROM "ForumAccount";
+DROP TABLE "ForumAccount";
+ALTER TABLE "new_ForumAccount" RENAME TO "ForumAccount";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
