0a1bb16a9ead96d502d84d740aedd5dc3136951d
Dmitriy K <dmitriykud-07@bk.ru>
2024-03-14 22:00:51 +0300

    fix error v2

--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -1,63 +1,84 @@
-import {cookies} from "next/headers";
-import {PrismaClient} from "@prisma/client";
+import { cookies } from "next/headers";
+import { PrismaClient } from "@prisma/client";
 // @ts-ignore
 import jwt from "jsonwebtoken";
-import {NextResponse} from "next/server";
+import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
-    const {body} = req
-    const prisma = new PrismaClient()
+  const { body } = req;
+  const prisma = new PrismaClient();
 
-    if (cookies().has("token")) {
-        try {
-            const token = jwt.verify(cookies().get("token")?.value, "lssd-scrapper-2024")
-            if (token) {
+  if (cookies().has("token")) {
+    try {
+      const token = jwt.verify(
+        cookies().get("token")?.value,
+        "lssd-scrapper-2024"
+      );
+      if (token) {
+        const forAllTemplates = await prisma.template.findMany({
+          where: {
+            OR: [
+              {
+                forAll: true,
+              },
 
-                const forAllTemplates = await prisma.template.findMany({
-                    where: {
-                        forAll: true
-                    }
-                })
-
-                const user = await prisma.user.findFirst({
-                    where: {
-                        id: token.id
-                    },
-                    include: {
-                        role: {
-                            include: {
-                                permissions: true
-                            }
-                        },
-                        forumAccounts: true,
-                        invitesCreated: true,
-                        templatesCreated: true,
-                        activeInvite: {
-                            include: {
-                                created: true
-                            }
-                        }
-                    }
-                })
-                if (user) {
-                    return NextResponse.json({
-                        ...user,
-                        templates: [...user.templatesCreated, ...forAllTemplates],
-                    })
+              {
+                NOT: {
+                  createdId: token.id,
+                },
+              },
+            ],
+          },
+          include: {
+            created: {
+                select: {
+                    id: true
                 }
-
-
             }
-        } catch (e) {
-            return NextResponse.json({error: e}, {
-                status: 403
-            });
+          },
+        });
+
+        const user = await prisma.user.findFirst({
+          where: {
+            id: token.id,
+          },
+          include: {
+            role: {
+              include: {
+                permissions: true,
+              },
+            },
+            forumAccounts: true,
+            invitesCreated: true,
+            templatesCreated: true,
+            activeInvite: {
+              include: {
+                created: true,
+              },
+            },
+          },
+        });
+        if (user) {
+          return NextResponse.json({
+            ...user,
+            templates: [...forAllTemplates],
+          });
         }
-
+      }
+    } catch (e) {
+      return NextResponse.json(
+        { error: e },
+        {
+          status: 403,
+        }
+      );
     }
+  }
 
-    return NextResponse.json({error: "Forbidden"}, {
-        status: 403
-    })
-
+  return NextResponse.json(
+    { error: "Forbidden" },
+    {
+      status: 403,
+    }
+  );
 }
